142082ff70527b15a07b4894976d7352803052cf
MikeTDS <m_ilski@interia.pl>
2019-10-31 14:40:12 +0100

    Dziala juz liczenie Vat w PLN

--- src/main/java/billingappspackage/OrderedProduct.java
+++ src/main/java/billingappspackage/OrderedProduct.java
@@ -6,7 +6,14 @@ public class OrderedProduct extends Product {
     public OrderedProduct(String _name, float _nettoPrice, int _amount){
         super(_name, _nettoPrice);
         amount=_amount;
-        totalBruttoPrice = ((int)(getBruttoPrice()*amount*100))/100f;
-        totalNettoPrice = ((int)(getNettoPrice()*amount*100))/100f;
+        totalNettoPrice = _nettoPrice*amount;
+        totalBruttoPrice = calculateTotalBruttoPrice();
+    }
+    public float getTotalBruttoPrice() { return totalBruttoPrice; }
+    public float getTotalNettoPrice() { return totalNettoPrice; }
+    private float calculateTotalBruttoPrice(){
+        int gr=getBruttoGr();
+        int zl=getBruttoZl();
+        return (((amount*gr)%100f)/100f)+amount*zl+((amount*gr)/100);
     }
 }
--- src/main/java/billingappspackage/Product.java
+++ src/main/java/billingappspackage/Product.java
@@ -3,12 +3,17 @@ package billingappspackage;
 public class Product {
     private String name;
     private float nettoPrice, bruttoPrice;
+    private int nettoZl, nettoGr, bruttoZl, bruttoGr;
     public Product(String _name, float _nettoPrice){
         name=_name;
         nettoPrice=_nettoPrice;
         bruttoPrice=_nettoPrice*(1.23f);
+        bruttoZl=(int)bruttoPrice;
+        bruttoGr=(Math.round((bruttoPrice-bruttoZl)*100));
     }
     public String getName(){ return name; }
     public float getNettoPrice() { return nettoPrice; }
     public float getBruttoPrice() { return bruttoPrice; }
+    public int getBruttoGr() { return bruttoGr; }
+    public int getBruttoZl() { return bruttoZl; }
 }
